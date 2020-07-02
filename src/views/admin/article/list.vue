c99109e306d38407b7dd06e1100460e2c8e79307
ChoSaJang <enjoy.hyun@gmail.com>
2020-07-03 01:00:31 +0900

    - post에서 article로 변경

--- src/views/admin/article/list.vue
+++ src/views/admin/article/list.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="content">
-    <p>Admin / Post</p>
+    <p>Admin / Article</p>
     <ul>
       <li>작성된 글 목록 조회</li>
       <li>글 쓰기 / 수정 / 삭제</li>
     </ul>
+    <input type="button" class="btn btn-primary" value="글쓰기" @click="location.href='/admin/article'" />
   </div>
 </template>
 
