2cb944563285f0e6655e791cbb1283ddbea5f928
ChoSaJang <enjoy.hyun@gmail.com>
2020-07-05 03:50:44 +0900

    - 게시물 목록 페이지 작업

--- src/views/admin/article/list.vue
+++ src/views/admin/article/list.vue
@@ -1,11 +1,45 @@
 <template>
   <div class="content">
-    <p>Admin / Article</p>
-    <ul>
-      <li>작성된 글 목록 조회</li>
-      <li>글 쓰기 / 수정 / 삭제</li>
-    </ul>
-    <input type="button" class="btn btn-primary" value="글쓰기" @click="location.href='/admin/article'" />
+    <div class="functionWrap">
+      <!-- SelectBox -->
+      <input type="text" v-model="search" placeholder="Board Name"/>
+    </div>
+    <div class="itemWrap table-responsive">
+      <!-- Item List -->
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">게시물 이름</th>
+            <th scope="col">작성자</th>
+            <th scope="col">등록일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in itemListSlice" v-bind:key="item.SEQ" @click="boardInfoPop(item)">
+            <th scope="row">{{ (pageNum * 10) + index + 1 }}</th>
+            <td>{{ item.NAME }}</td>
+            <td>{{ item.ATTACHED_FILE_YN }}</td>
+            <td>{{ item.COMMENT_YN }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <!-- Page Navigation -->
+      <nav aria-label="Page navigation">
+        <paginate
+          :pageCount="pageCount"
+          :clickHandler="pageMove"
+          :containerClass="'pagination justify-content-center'"
+          :prev-class="'page-item'"
+          :prev-link-class="'page-link'"
+          :page-class="'page-item'"
+          :page-link-class="'page-link'"
+          :next-class="'page-item'"
+          :next-link-class="'page-link'">
+        </paginate>
+      </nav>
+      <input type="button" class="btn btn-primary" value="게시판 생성" @click="boardInfoPop(null)" />
+    </div>
   </div>
 </template>
 
@@ -15,5 +49,60 @@ export default {
 </script>
 
 <style>
+/**
+ * common
+ */
+.functionWrap {
+  padding-top: 20px;
+  width: 100%;
+}
 
+.itemWrap {
+  padding-top: 20px;
+}
+
+.itemWrap > .title {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.itemWrap > table > thead tr, tbody tr{
+  text-align: center;
+}
+
+input[type=text] {
+  width: 220px;
+  padding: 8px 14px;
+  margin: 4px 0;
+  font-size: 14px;
+  font-weight: bold;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 3px solid #2D353C;
+}
+
+input[type=text]:focus {
+  outline: none;
+  transition: .2s;
+  color: #608BCB;
+  border-bottom: 3px solid #608BCB;
+}
+
+input[type=text]:focus::placeholder {
+  color: #608BCB;
+  transition: .2s;
+}
+
+/**
+ * Mobile
+ */
+@media all and (max-width:768px) {
+
+}
+/**
+ * Tablet & Desktop
+ */
+@media all and (min-width:768px) {
+
+}
 </style>
