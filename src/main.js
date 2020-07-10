84f3aed40b2e042d4abe7bde66925345a3504772
ChoSaJang <enjoy.hyun@gmail.com>
2020-07-11 01:28:09 +0900

    - vue-moment 추가

--- src/main.js
+++ src/main.js
@@ -9,12 +9,14 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Paginate from 'vuejs-paginate';
 import store from './store/index.js';
+import VueMoment from 'vue-moment';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionApi);
 Vue.use(Bootstrap);
 Vue.use(VueSweetalert2);
+Vue.use(VueMoment);
 Vue.component('paginate', Paginate);
 
 new Vue({
