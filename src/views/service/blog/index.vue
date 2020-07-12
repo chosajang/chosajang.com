e2fc3436706a958815ca002e5c564c24224fc9a1
ChoSaJang <enjoy.hyun@gmail.com>
2020-07-13 01:34:20 +0900

    - 게시물 목록 레이아웃 작업

--- src/views/service/blog/index.vue
+++ src/views/service/blog/index.vue
@@ -4,10 +4,13 @@
       공부하고 만들며 경험한 것을 정리하여 나누는 공간 :)
     </div>
     <div class="list">
-      <div v-for="item in itemList" v-bind:key="item.SEQ" :value="item.SEQ">
-        <div class="thumbnail">이미지</div>
-        <div class="title">제목 : {{item.TITLE}}</div>
-        <div class="info"><i class="far fa-calendar-alt"></i> {{$moment(item.ADD_DATE).format('YYYY.MM.DD')}}</div>
+      <div class="post" v-for="item in itemList" v-bind:key="item.SEQ" :value="item.SEQ">
+        <div class="image">이미지</div>
+        <div class="text">
+          <div class="title">{{item.TITLE}}</div>
+          <div class="summary">내용 요약본이 여기에 출력됩니다 최대 2줄까지를 기획중이며, 글이 넘어가는 경우 마지막에 ...으로 표시하여 화면이 깨지는 것을 방지할 수 있도록 합니다</div>
+          <div class="info"><i class="far fa-calendar-alt"></i> {{$moment(item.ADD_DATE).format('YYYY.MM.DD')}}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -64,6 +67,38 @@ export default {
   width: 900px;
   margin: 0 auto;
   background-color: cyan;
+}
+
+.content > .list > .post {
+  display: flex;
+  justify-content: start;
+  margin: 20px 0px 20px 0px;
+  height: 140px;
+  background-color: #FFF;
+}
+
+.post > .image {
+  width: 200px;
+  height: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.post > .text {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 10px 0 10px 0;
+}
+
+.text > .title {
+  font-size: 22px;
+}
+
+.text > .summary {
+  font-size: 16px;
+  margin: 6px 0;
 }
 
 </style>
