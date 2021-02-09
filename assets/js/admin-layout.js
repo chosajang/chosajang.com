a1175a622fa8acb846e03b2ec8b576a4345303e4
ChoSaJang <enjoy.hyun@gmail.com>
2021-02-09 23:41:23 +0900

    - 사이드바 이벤트 처리, 디자인 요소 삽입

--- assets/js/admin-layout.js
+++ assets/js/admin-layout.js
@@ -4,24 +4,19 @@
  * DOM 트리만 완성되면 바로 실행하는 이벤트
  */
 document.addEventListener("DOMContentLoaded", function(){
-  // 이벤트 객체 생성
-  // let customEvent = document.createEvent('Event');
-  // customEvent.initEvent('click', true, false);
   
   // 메뉴 펼침/닫힘 이벤트
-  let menuObj = document.getElementById('menu-ext');
-  menuObj.addEventListener('click',(thisObj) => {
-    // 클릭한 요소의 dom 요소 접근
-    const cObj = thisObj.path[0];
-    const iObj = cObj.children[0];
-    console.log(iObj);
-    const extMenuObj = document.getElementById(cObj.getAttribute('for'));
-    console.log(extMenuObj);
-    if(extMenuObj != null ){
-      extMenuObj.classList.toggle('hidden');
-      iObj.classList.toggle('fa-rotate-180');
-    }
-  }, false);
+  let liList = document.getElementById('menu-ext').querySelectorAll("li");
+  liList.forEach( liObj => {
+    liObj.addEventListener('click', () => {
+      const extMenuObj = document.getElementById(liObj.getAttribute('for'));
+      if(extMenuObj != null ){
+        extMenuObj.classList.toggle('hidden');
+        const iObj = liObj.children[1].querySelector("i")
+        iObj.classList.toggle('fa-rotate-90');
+      }
+    });
+  });
 
   // 이벤트 실행(준비)
   // menuObj.dispatchEvent(customEvent);
