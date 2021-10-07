7a5ca94226aee472b1a767b17b786a362415b155
jonny <enjoy.hyun@gmail.com>
2021-10-07 16:44:53 +0900

    prerender 설정 주석처리

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,29 @@
-const path = require('path');
-const PrerenderSpaPlugin = require('prerender-spa-plugin');
+// const path = require('path');
+// const PrerenderSpaPlugin = require('prerender-spa-plugin');
 
-const productionPlugins = [
-    new PrerenderSpaPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: [
-            '/blog/',
-            '/blog/1/',
-            '/blog/2/'
-        ],
-        server: {
-            port: 80,
-        },
-        renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
-            renderAfterElementExists: '#app',
-        }),
-    })
-];
+// const productionPlugins = [
+//     new PrerenderSpaPlugin({
+//         staticDir: path.join(__dirname, 'dist'),
+//         routes: [
+//             '/blog/',
+//             '/blog/1/',
+//             '/blog/2/'
+//         ],
+//         server: {
+//             port: 80,
+//         },
+//         renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+//             renderAfterElementExists: '#app',
+//         }),
+//     })
+// ];
 
 module.exports = {
     lintOnSave: false,
     publicPath: '/',
-    configureWebpack: (config) => {
-        if (process.env.NODE_ENV === 'production') {
-            config.plugins.push(...productionPlugins);
-        }
-    },
+    // configureWebpack: (config) => {
+    //     if (process.env.NODE_ENV === 'production') {
+    //         config.plugins.push(...productionPlugins);
+    //     }
+    // },
 };
